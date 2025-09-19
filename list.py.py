be92eeae620bb8ac2ce5b9b417ba36e9761d3dda
arjun78R <arju63191@gmail.com>
2025-09-19 10:56:38 +0530

    Add files via upload

---
+++ list.py.py
@@ -0,0 +1,31 @@
+list1=[4,5,6,7,8]
+list2=[20,30,40,5,6,70]
+
+
+if len(list1) == len(list2):
+    print("The Lists are of same size")
+else:
+    print("Not Same")
+
+
+
+sum1=0
+for i in list1:
+    sum1=sum1+i
+print("list1 sum = ",sum1)
+sum2=0
+for i in list2:
+    sum2=sum2+i
+print("list2 sum = ",sum2)
+if sum1 == sum2:
+    print("The sum are of same size")
+else:
+    print("Not Same")
+
+
+lst=[]
+for i in list1:
+    for j in list2:
+        if i == j :
+            lst.append(i)
+print(lst)
